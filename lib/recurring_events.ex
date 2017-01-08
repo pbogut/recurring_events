8f908c51c39e3a0a31000dc7f17596a96717309c
Pawel Bogut <pbogut@smeagol.pl>
2017-01-08 15:55:00 +0000

    Basic validation

--- lib/recurring_events.ex
+++ lib/recurring_events.ex
@@ -1,2 +1,11 @@
 defmodule RecurringEvents do
+
+  def unfold(_date, %{count: _, until: _}), do:
+    {:error, "Can have eathier, count or until"}
+
+  def unfold(_date, %{freq: _}) do
+    {:ok, []}
+  end
+
+  def unfold(_date, _rrule), do: {:error, "Frequency is missing"}
 end
--- test/recurring_events_test.exs
+++ test/recurring_events_test.exs
@@ -2,7 +2,21 @@ defmodule RecurringEventsTest do
   use ExUnit.Case
   doctest RecurringEvents
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @date ~N[2017-01-01 10:00:00]
+  @valid_rrule %{freq: :weekly}
+
+  test "freq is required" do
+    assert {:error, _} =
+      RecurringEvents.unfold(@date, %{})
+  end
+
+  test "can have eathier until or count" do
+    assert {:error, _} =
+      RecurringEvents.unfold(@date,
+                             Map.merge(@valid_rrule, %{count: 1, until: 2}))
+    assert {:ok, _} =
+      RecurringEvents.unfold(@date, Map.put(@valid_rrule, :until, 2))
+    assert {:ok, _} =
+      RecurringEvents.unfold(@date, Map.put(@valid_rrule, :count, 1))
   end
 end
