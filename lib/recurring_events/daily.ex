a9bd19f71c7aa9eb2fa32b64f5ee68129fe61b4d
Pawel Bogut <pbogut@smeagol.pl>
2017-01-15 22:38:31 +0000

    Add daily frequency

---
+++ lib/recurring_events/daily.ex
@@ -0,0 +1,50 @@
+defmodule RecurringEvents.Daily do
+  alias RecurringEvents.Date
+
+  def unfold(date, %{freq: :daily} = params, range) do
+    {:ok, do_unfold(date, params, range)}
+  end
+
+  def unfold!(date, %{freq: :daily} = params, range) do
+    do_unfold(date, params, range)
+  end
+
+  defp do_unfold(date, %{} = params, {from_date, to_date}) do
+    stop_date = get_stop_date(params, to_date)
+    count = get_count(params)
+    interval = get_interval(params)
+
+    get_days_stream(date, interval)
+    |> Enum.reduce_while([], fn date, acc ->
+      if Enum.count(acc) == count or :gt == Date.compare(date, stop_date) do
+        {:halt, acc}
+      else
+        {:cont, acc ++ [date]}
+      end
+    end)
+    |> Enum.drop_while(fn date ->
+      date < from_date
+    end)
+  end
+
+  defp get_days_stream(date, step) do
+    Stream.iterate(date, fn next_date ->
+      Date.shift_date(next_date, step, :days)
+    end)
+  end
+
+  defp get_stop_date(%{until: until}, to_date) do
+    if :lt == Date.compare(until, to_date) do
+      until
+    else
+      to_date
+    end
+  end
+  defp get_stop_date(%{}, to_date), do: to_date
+
+  defp get_interval(%{interval: interval}), do: interval
+  defp get_interval(%{}), do: 1
+
+  defp get_count(%{count: count}), do: count
+  defp get_count(%{}), do: :infinity
+end
---
+++ test/recurring_events/daily_test.exs
@@ -0,0 +1,37 @@
+defmodule RecurringEvents.DailyTest do
+  use ExUnit.Case
+  doctest RecurringEvents
+
+  alias RecurringEvents.Daily
+
+  @date ~N[2017-12-28 10:00:00]
+  @range {~N[2017-12-28 00:00:00], ~N[2018-02-01 23:59:59]}
+  @valid_rrule %{freq: :daily}
+
+  test "for count 1 should return only one event" do
+    assert {:ok, [@date]} ==
+      Daily.unfold(@date, @valid_rrule |> Map.put(:count, 1), @range)
+  end
+
+  test "for until ~N[2017-12-29 00:00:00] it should return 2 events" do
+    until = ~N[2017-12-29 00:00:00]
+    {:ok, events} = Daily.unfold(@date, @valid_rrule |> Map.put(:until, until), @range)
+    assert 2 == Enum.count(events)
+    assert [@date, %{@date | day: 29}] == events
+  end
+
+  test "with no count, until and interval it should return 35 events (1 for each day)" do
+    {:ok, events} = Daily.unfold(@date, @valid_rrule, @range)
+    assert 36 == Enum.count(events)
+  end
+
+  test "for count 5 it should return 5 events" do
+    {:ok, events} = Daily.unfold(@date, @valid_rrule |> Map.put(:count, 5), @range)
+    assert 5 == Enum.count(events)
+  end
+
+  test "for interval 5 it should return 4 events" do
+    {:ok, events} = Daily.unfold(@date, @valid_rrule |> Map.put(:interval, 5), @range)
+    assert 8 == Enum.count(events)
+  end
+end
