eb8fea0443b40fe68be95dd6c592942fe422050b
Pawel Bogut <pbogut@smeagol.pl>
2017-01-18 07:33:33 +0000

    Add implemented frequencies to main modul

--- lib/recurring_events.ex
+++ lib/recurring_events.ex
@@ -1,11 +1,20 @@
 defmodule RecurringEvents do
+  alias RecurringEvents.{Yearly, Monthly, Daily}
+  use RecurringEvents.Guards
 
-  def unfold(_date, %{count: _, until: _}), do:
+
+  def unfold(_date, %{count: _, until: _}, _range) do
     {:error, "Can have eathier, count or until"}
-
-  def unfold(_date, %{freq: _}) do
-    {:ok, []}
   end
+  def unfold(date, %{freq: freq} = params, range) when is_freq_valid(freq) do
+    get_freq_module(freq).unfold(date, params, range)
+  end
+  def unfold(_date, %{freq: _}, _range), do: {:error, "Frequency is invalid"}
+  def unfold(_date, _rrule, _range), do: {:error, "Frequency is missing"}
 
-  def unfold(_date, _rrule), do: {:error, "Frequency is missing"}
+
+  defp get_freq_module(:yearly), do: Yearly
+  defp get_freq_module(:monthly), do: Monthly
+  defp get_freq_module(:daily), do: Daily
+
 end
--- test/recurring_events_test.exs
+++ test/recurring_events_test.exs
@@ -3,20 +3,41 @@ defmodule RecurringEventsTest do
   doctest RecurringEvents
 
   @date ~N[2017-01-01 10:00:00]
-  @valid_rrule %{freq: :weekly}
+  @valid_rrule %{freq: :yearly}
+  @range_3y {~D[2017-01-01], ~D[2019-12-31]}
+  @range_3m {~D[2017-01-01], ~D[2017-03-31]}
 
   test "freq is required" do
     assert {:error, _} =
-      RecurringEvents.unfold(@date, %{})
+      RecurringEvents.unfold(@date, %{}, @range_3y)
   end
 
   test "can have eathier until or count" do
     assert {:error, _} =
       RecurringEvents.unfold(@date,
-                             Map.merge(@valid_rrule, %{count: 1, until: 2}))
+                             Map.merge(@valid_rrule, %{count: 1, until: 2}),
+                             @range_3y)
     assert {:ok, _} =
-      RecurringEvents.unfold(@date, Map.put(@valid_rrule, :until, 2))
+      RecurringEvents.unfold(@date, Map.put(@valid_rrule, :until, @date), @range_3y)
     assert {:ok, _} =
-      RecurringEvents.unfold(@date, Map.put(@valid_rrule, :count, 1))
+      RecurringEvents.unfold(@date, Map.put(@valid_rrule, :count, 1), @range_3y)
+  end
+
+  test "can handle yearly frequency" do
+    assert {:ok, events}  =
+      RecurringEvents.unfold(@date, %{freq: :yearly}, @range_3y)
+    assert 3 = Enum.count(events)
+  end
+
+  test "can handle monthly frequency" do
+    assert {:ok, events}  =
+      RecurringEvents.unfold(@date, %{freq: :monthly}, @range_3y)
+    assert 36 = Enum.count(events)
+  end
+
+  test "can handle daily frequency" do
+    assert {:ok, events}  =
+      RecurringEvents.unfold(@date, %{freq: :daily}, @range_3m)
+    assert 90 = Enum.count(events)
   end
 end
