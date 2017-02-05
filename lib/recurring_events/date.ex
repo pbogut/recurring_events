03fc0812d86b47567be44714393390f40040101c
Pawel Bogut <pbogut@smeagol.pl>
2017-02-05 01:48:00 +0000

    Add documentation

--- lib/recurring_events/date.ex
+++ lib/recurring_events/date.ex
@@ -1,4 +1,7 @@
 defmodule RecurringEvents.Date do
+  @moduledoc """
+  Helper module responsible for common date manipulations.
+  """
 
   @week_days [
     :monday,
@@ -10,6 +13,18 @@ defmodule RecurringEvents.Date do
     :sunday
   ]
 
+  @doc """
+  Shifts date by `:days`, `:weeks`, `:months` and `:years`
+
+  # Example
+
+      iex> RecurringEvents.Date.shift_date(~D[2011-02-04], 4, :days)
+      ~D[2011-02-08]
+
+      iex> RecurringEvents.Date.shift_date(~D[2011-02-04], 2, :years)
+      ~D[2013-02-04]
+
+  """
   def shift_date(%{year: year, month: month, day: day} = date, count, period) do
     {new_year, new_month, new_day} =
       shift_date({year, month, day}, count, period)
@@ -43,6 +58,15 @@ defmodule RecurringEvents.Date do
     {year + count, month, day}
   end
 
+  @doc """
+  Returns last daty of the month for provided date.
+
+  # Example
+
+      iex> RecurringEvents.Date.last_day_of_the_month(~D[2017-02-04])
+      28
+
+  """
   def last_day_of_the_month(%{year: year, month: month}) do
     :calendar.last_day_of_the_month(year, month)
   end
@@ -51,6 +75,15 @@ defmodule RecurringEvents.Date do
     :calendar.last_day_of_the_month(year, month)
   end
 
+  @doc """
+  Returns week day of provided date
+
+  # Example
+
+      iex> RecurringEvents.Date.week_day(~D[2017-02-04])
+      :saturday
+
+  """
   def week_day(%{year: year, month: month, day: day}) do
     week_day({year, month, day})
   end
@@ -59,6 +92,15 @@ defmodule RecurringEvents.Date do
     @week_days |> Enum.at(:calendar.day_of_the_week(date) - 1)
   end
 
+  @doc """
+  Shifts week days
+
+  # Example
+
+      iex> RecurringEvents.Date.shift_week_day(:monday, -3)
+      :friday
+
+  """
   def shift_week_day(day, shift) do
     day_no =
       @week_days
@@ -69,14 +111,48 @@ defmodule RecurringEvents.Date do
     Enum.at(@week_days, day_no)
   end
 
+  @doc """
+  Returns next day of the week
+
+  # Example
+
+      iex> RecurringEvents.Date.next_week_day(:friday)
+      :saturday
+
+  """
   def next_week_day(day) do
     shift_week_day(day, 1)
   end
 
+  @doc """
+  Returns previous day of the week
+
+  # Example
+
+      iex> RecurringEvents.Date.prev_week_day(:wednesday)
+      :tuesday
+
+  """
   def prev_week_day(day) do
     shift_week_day(day, -1)
   end
 
+  @doc """
+  Compares two dates, time if provided will be ignored.
+
+  # Example
+
+      iex> RecurringEvents.Date.compare(~D[2017-02-05], ~D[2017-02-01])
+      :gt
+
+      iex> RecurringEvents.Date.compare(~D[2017-02-01], ~D[2017-02-05])
+      :lt
+
+      iex> RecurringEvents.Date.compare(~N[2017-02-05 12:00:00],
+      ...>                              ~N[2017-02-05 18:21:11])
+      :eq
+
+  """
   def compare(%{year: y1, month: m1, day: d1},
               %{year: y2, month: m2, day: d2}) do
     compare({y1, m1, d1}, {y2, m2, d2})
